b485486fb7cd22ef41c9f676904a39ffd771495f
Faizal-work <faizal.work@proton.me>
2026-04-29 17:46:22 +0800

    HTML is now dynamic

---
+++ src/articles/articles.js
@@ -0,0 +1,22 @@
+const articles = [
+  {
+    "date": "23 October 2025",
+    "title": "Dependency Injection: Production Patterns",
+    "url": "./articles/2025/20251023_dependency_injection_production_patterns.html"
+  },
+  {
+    "date": "22 October 2025",
+    "title": "Casette Futurism: Does it speak to me?",
+    "url": "./articles/2025/20251022_casette_futurism.html"
+  },
+  {
+    "date": "19 October 2025",
+    "title": "ASP.NET Core Essentials: Quick Reference",
+    "url": "./articles/2025/20251019_asp_dot_net_reference.html"
+  },
+  {
+    "date": "07 November 2025",
+    "title": "Dependency Injection: Service Lifetimes Explained",
+    "url": "./articles/2025/20251107_asp_net_readiness.html"
+  }
+]
